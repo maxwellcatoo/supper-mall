540b15ba5ebe7677eb2d1226a06e64bb3e5f2d3e
maxwellcatoo <50536526+maxwellcatoo@users.noreply.github.com>
2020-08-02 23:04:01 +0800

    路径修改

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
     //1.创建axios实例
     const instance = axios.create({
         // baseURL: "http://123.207.32.32:8000",
-        baseURL: 'http://152.136.185.210:8000/api/z8',
+        baseURL: 'http://152.136.185.210:8000/api/a6',
         timeout: 10000
     })
     //2.axios拦截器
